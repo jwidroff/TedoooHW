8171f9736fdabea0a7040b8dbddee4fbe36d8a4f
jeffery widroff <jeff.widroff@gmail.com>
2022-06-06 13:06:18 +0300

    Initial Commit

---
+++ TedoooHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TedoooHW
+//
+//  Created by Jeffery Widroff on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
